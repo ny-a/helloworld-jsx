46f198b71a1c92cc5ee7a2c205390995784da711
ny-a <58758248+ny-a@users.noreply.github.com>
2020-01-20 21:35:06 +0900

    Add eslint and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,37 @@
 function Headline() {
-    return (
-        <>
-            <h1 className="headline">Hello this in an h1
-                <br/>
-                new line
-            </h1>
-            <h2>Second Headline</h2>
-        </>
-    )
+  return (
+    <>
+      <h1 className="headline">
+        Hello this in an h1
+        <br />
+        new line
+      </h1>
+      <h2>Second Headline</h2>
+    </>
+  );
 }
 
 function Main() {
-    return (
-        <div>
-            <Headline/>
-            <p>Lorem ipsum</p>
-            <ul>
-                <li><a href="">anchor</a></li>
-                <li>More</li>
-            </ul>
-            <button ref={node => {
-                node.addEventListener('click', console.log)
-            }}>
-                Click Me!
-            </button>
-        </div>
-    )
+  return (
+    <div>
+      <Headline />
+      <p>Lorem ipsum</p>
+      <ul>
+        <li>
+          <a href="">anchor</a>
+        </li>
+        <li>More</li>
+      </ul>
+      <button
+        ref={node => {
+          // eslint-disable-next-line no-console
+          node.addEventListener('click', console.log);
+        }}
+      >
+        Click Me!
+      </button>
+    </div>
+  );
 }
 
 const app = document.querySelector('#root');
